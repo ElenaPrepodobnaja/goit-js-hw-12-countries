4b0adfe41f669423611944cdda8f16871093bcc1
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-05-27 22:51:53 +0300

    12-4

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,9 @@
-const BASE_URL = 'https:restcountries.eu/rest/v2/name'
+const BASE_URL = 'https:restcountries.eu/rest/v2'
 
 function fetchCountry(country) {
-    return fetch(`${BASE_URL}/${country}`)
-        .then(response => {
-            return response.json();
-        })
+    return fetch(`${BASE_URL}/name/${country}`)
+        .then(response =>
+            response.json(),
+        );
 };
 export default { fetchCountry };
