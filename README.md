b8050c50c3c4e614e175d1373692000f708e7b8d
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-05-27 20:42:57 +0300

    ДЗ 12

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # goit-js-hw-12-countries
-goit-js-hw-12-countries
+
+https://elenaprepodobnaja.github.io/goit-js-hw-12-countries/
--- src/fetchCountries.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = "https://restcountries.eu/rest/v2/name/";
-
-async function fetchCountries(searchQuery) {
-  return await (await fetch(`${BASE_URL}${searchQuery}`)).json();
-}
-
-export default { fetchCountries };
